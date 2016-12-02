8fed777230540fb7dd22082a1bbfadd6bbb5b656
Robert Monden <cj_robert@outlook.com>
2016-12-02 10:06:13 +0100

    Initial commit

---
+++ bin/main/Main.java
@@ -0,0 +1,26 @@
+/**
+ * PROJECT ALPHA | Open source hotel management software
+ *
+ * Main.java
+ * @since December 2, 2016
+ * @package main
+ * 
+ * @version 1.0.0
+ * @authors Rick Nieborg, Mark Nieborg & Robert Monden
+ * @copyright 2016 Project Alpha
+ * 
+ * @license MIT
+ */
+
+package main;
+
+public class Main {
+	
+	/**
+	 * This class is called automatically
+	 * @param args
+	 */
+	public static void main (String[] args) {
+		System.out.print("Hello.");
+	}
+}
