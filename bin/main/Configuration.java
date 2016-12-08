eb1892979ca69d1d19665f6b074dc6a1a022f15d
Robert Monden <cj_robert@outlook.com>
2016-12-08 08:35:33 +0100

    Adding configuration file and class blueprint

---
+++ bin/main/Configuration.java
@@ -0,0 +1,30 @@
+/**
+ * PROJECT ALPHA | Open source hotel management software
+ *
+ * Configuration.java
+ * @since December 8, 2016
+ * @package main
+ * 
+ * @version 1.0.0
+ * @authors Rick Nieborg, Mark Nieborg & Robert Monden
+ * @copyright 2016 Project Alpha
+ * 
+ * @license MIT
+ */
+
+package main;
+
+/**
+ * This class loads the configuration file and makes sure the settings
+ * in that file can be used by the software.
+ */
+public class Configuration {
+	
+	/**
+	 * The constructor. Currently, it does absolutely nothing.
+	 * @return void
+	 */
+	public Configuration() {
+		
+	}
+}
