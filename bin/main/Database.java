372b1e707396322a0db29cb1e0e1e2087547a4b0
Robert Monden <cj_robert@outlook.com>
2016-12-09 11:30:59 +0100

    Database connection

--- bin/main/Database.java
+++ bin/main/Database.java
@@ -35,14 +35,15 @@ public class Database {
 	public void connect() {
 		try {
 			connection = DriverManager.getConnection(
-					"jdbc:mysql://" + Configuration.config.getProperty("db_server")
-						+ ":" + Configuration.config.getProperty("db_port")
-						+ "/" + Configuration.config.getProperty("db_name"),
-					Configuration.config.getProperty("db_username"), 
-					Configuration.config.getProperty("db_password"));
+				"jdbc:mysql://" + Configuration.config.getProperty("db_server")
+					+ ":" + Configuration.config.getProperty("db_port")
+					+ "/" + Configuration.config.getProperty("db_name")
+					+ "?useSSL=false",
+				Configuration.config.getProperty("db_username"),
+				Configuration.config.getProperty("db_password"));
 		}
 		catch (SQLException e) {
-			System.out.println("Could not establish a connection with the database. Detailed error message:");
+			System.out.println("Could not establish a connection to the database. Detailed error message:");
 			e.printStackTrace();
 			System.exit(0);
 		}
