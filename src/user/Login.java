0df1cfc96b94f56596ba3e2b22cc83b093cc04ea
Robert Monden <cj_robert@outlook.com>
2016-12-28 14:18:36 +0100

    Adding login screen (still a WIP)

--- src/user/Login.java
+++ src/user/Login.java
@@ -15,6 +15,10 @@
 package user;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -30,11 +34,35 @@ public class Login {
 
     public Stage getStage(Stage stage) {
         _stage = stage;
-        _layout = new GridPane();
-        _scene = new Scene(_layout, 500, 350);
+        _stage.setResizable(false);
 
+        _layout = new GridPane();
+        _layout.setVgap(10);
+        _layout.setHgap(10);
+
+        // Labels should go below this line
+        Label labelUsername = new Label("Username:");
+        GridPane.setConstraints(labelUsername, 3, 3);
+
+        Label labelPassword = new Label("Password:");
+        GridPane.setConstraints(labelPassword, 3, 4);
+
+        // Input fields should go below this line
+        TextField fieldUsername = new TextField();
+        GridPane.setConstraints(fieldUsername, 4, 3);
+        fieldUsername.setPromptText("Username");
+
+        PasswordField fieldPassword = new PasswordField();
+        GridPane.setConstraints(fieldPassword, 4, 4);
+
+        Button submitButton = new Button("Log in");
+        GridPane.setConstraints(submitButton, 4, 5);
+
+        _layout.getChildren().addAll(labelUsername, labelPassword, fieldUsername, fieldPassword, submitButton);
+
+        _scene = new Scene(_layout, 300, 150);
         _stage.setScene(_scene);
-        _stage.setTitle("Project Alpha | Employee login");
+        _stage.setTitle("Employee login");
         return _stage;
     }
 }
